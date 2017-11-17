3c71558bff633b1ea129c11393bf5601ac229ddc
asyrov <asyrovprog@gmail.com>
2017-11-16 22:08:28 -0800

    modified:   .idea/workspace.xml 	modified:   calcs/la-fire-rnn-smoke.data-00000-of-00001 	modified:   calcs/la-fire-rnn-smoke.index 	modified:   calcs/la-fire-rnn-smoke.meta 	modified:   dataset/la-fire-info.py.ipynb 	new file:   dataset/san-francisco-fire-info.py.ipynb 	renamed:    dataset/la-fire-preprocess.py -> dataset/san-francisco-fire-preprocess.py 	renamed:    dataset/la-fire-rnn-smoke.py -> dataset/san-francisco-fire-rnn-smoke.py 	modified:   download_datasets.py 	renamed:    ds/los-angeles-fire-counts-2001-2017-hours-gaussian.csv -> ds/san-francisco-fire-counts-2001-2017-hours-gaussian.csv 	renamed:    ds/los-angeles-fire-counts-2001-2017-hours.csv -> ds/san-francisco-fire-counts-2001-2017-hours.csv 	renamed:    ds/los-angeles-fire-counts-2001-2017.csv -> ds/san-francisco-fire-counts-2001-2017.csv

--- dataset/san-francisco-fire-preprocess.py
+++ dataset/san-francisco-fire-preprocess.py
@@ -1,20 +1,20 @@
 from dataset.utils import *
 
 if __name__ == "__main__":
-    FROMFILE = "../data_raw/los-angeles-fire.csv"
-    TOFILE = "../ds/los-angeles-fire-counts-2001-2017.csv"
+    FROMFILE = "../data_raw/san-francisco-fire.csv"
+    TOFILE = "../ds/san-francisco-fire-counts-2001-2017.csv"
     print("Creating by day totals for incidents...")
     start_date = datetime(year=2001, month=1, day=1)
     end_date = datetime(year=2017, month=10, day=1)
     # preprocess(FROMFILE, TOFILE, 'Received DtTm', start_date, end_date)
     print("Saved to " + TOFILE)
 
-    TOFILE = "../ds/los-angeles-fire-counts-2001-2017-hours.csv"
+    TOFILE = "../ds/san-francisco-fire-counts-2001-2017-hours.csv"
     print("Creating by day totals for incidents...")
     # preprocess(FROMFILE, TOFILE, 'Received DtTm', start_date, end_date, False)
     print("Saved to " + TOFILE)
 
-    TOFILE = "../ds/los-angeles-fire-counts-2001-2017-hours-gaussian.csv"
+    TOFILE = "../ds/san-francisco-fire-counts-2001-2017-hours-gaussian.csv"
     print("Creating by day totals for incidents...")
     preprocess(FROMFILE, TOFILE, 'Received DtTm', start_date, end_date, False, True)
     print("Saved to " + TOFILE)
